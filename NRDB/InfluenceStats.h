656d1cddf706fa7434e4e596cf8aafc647793d5b
Gereon Steffens <gereon@steffens.org>
2014-02-17 20:42:01 +0100

    add influence stats, add "Mythic" to ICE types

---
+++ NRDB/InfluenceStats.h
@@ -0,0 +1,13 @@
+//
+//  InfluenceStats.h
+//  NRDB
+//
+//  Created by Gereon Steffens on 17.02.14.
+//  Copyright (c) 2014 Gereon Steffens. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface InfluenceStats : NSObject
+
+@end
---
+++ NRDB/InfluenceStats.m
@@ -0,0 +1,13 @@
+//
+//  InfluenceStats.m
+//  NRDB
+//
+//  Created by Gereon Steffens on 17.02.14.
+//  Copyright (c) 2014 Gereon Steffens. All rights reserved.
+//
+
+#import "InfluenceStats.h"
+
+@implementation InfluenceStats
+
+@end
