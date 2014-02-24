47bd0d3e3f8d7cc9618cf4df97a89745963bbb47
Gereon Steffens <gereon@steffens.org>
2014-02-24 07:27:35 +0100

    new tester devices added

--- NRDB/CardSets.m
+++ NRDB/CardSets.m
@@ -16,6 +16,7 @@
 @property NSString* settingsKey;
 @property NSString* setName;
 @property NRCycle cycle;
+@property BOOL released;
 @end
 
 @implementation CardSets
@@ -29,29 +30,30 @@ static struct cardSetData {
     char* setCode;
     char* setName;
     NRCycle cycle;
+    BOOL released;
 } cardSetData[] = {
-    {  1, "use_coreset", "core", "Core Set", NRCycleCoreDeluxe },
-    {  8, "use_creation_and_control", "cac", "Creation and Control", NRCycleCoreDeluxe },
-    { 15, "use_honor_and_profit", "hap", "Honor and Profit", NRCycleCoreDeluxe },
+    {  1, "use_coreset", "core", "Core Set", NRCycleCoreDeluxe, YES },
+    {  8, "use_creation_and_control", "cac", "Creation and Control", NRCycleCoreDeluxe, YES },
+    { 15, "use_honor_and_profit", "hap", "Honor and Profit", NRCycleCoreDeluxe, NO },
     
     // genesis
-    { 2, "use_what_lies_ahead", "wla", "What Lies Ahead", NRCycleGenesis },
-    { 3, "use_trace_amount", "ta", "Trace Amount", NRCycleGenesis },
-    { 4, "use_cyber_exodus", "ce", "Cyber Exodus", NRCycleGenesis },
-    { 5, "use_study_in_static", "asis", "A Study in Static", NRCycleGenesis },
-    { 6, "use_humanitys_shadow", "hs", "Humanity's Shadow", NRCycleGenesis },
-    { 7, "use_future_proof", "fp", "Future Proof", NRCycleGenesis },
+    { 2, "use_what_lies_ahead", "wla", "What Lies Ahead", NRCycleGenesis, YES },
+    { 3, "use_trace_amount", "ta", "Trace Amount", NRCycleGenesis, YES },
+    { 4, "use_cyber_exodus", "ce", "Cyber Exodus", NRCycleGenesis, YES },
+    { 5, "use_study_in_static", "asis", "A Study in Static", NRCycleGenesis, YES },
+    { 6, "use_humanitys_shadow", "hs", "Humanity's Shadow", NRCycleGenesis, YES },
+    { 7, "use_future_proof", "fp", "Future Proof", NRCycleGenesis, YES },
     
     // spin
-    {  9, "use_opening_moves", "om", "Opening Moves", NRCycleSpin },
-    { 10, "use_second_thoughts", "st", "Second Thoughts", NRCycleSpin },
-    { 11, "use_mala_tempora", "mt", "Mala Tempora", NRCycleSpin },
-    { 12, "use_true_colors", "tc", "True Colors", NRCycleSpin },
-    { 13, "use_fear_and_loathing", "fal", "Fear and Loathing", NRCycleSpin },
-    { 14, "use_double_time", "dt", "Double Time", NRCycleSpin },
+    {  9, "use_opening_moves", "om", "Opening Moves", NRCycleSpin, YES },
+    { 10, "use_second_thoughts", "st", "Second Thoughts", NRCycleSpin, YES},
+    { 11, "use_mala_tempora", "mt", "Mala Tempora", NRCycleSpin, YES },
+    { 12, "use_true_colors", "tc", "True Colors", NRCycleSpin, YES },
+    { 13, "use_fear_and_loathing", "fal", "Fear and Loathing", NRCycleSpin, YES },
+    { 14, "use_double_time", "dt", "Double Time", NRCycleSpin, NO },
     
     // lunar
-    { 16, "use_upstalk", "up", "Upstalk", NRCycleLunar },
+    { 16, "use_upstalk", "up", "Upstalk", NRCycleLunar, NO },
     { 0 }
 };
 
@@ -69,6 +71,7 @@ static struct cardSetData {
         csd.settingsKey = [NSString stringWithUTF8String:c->settingsKey];
         csd.setName = [NSString stringWithUTF8String:c->setName];
         csd.cycle = c->cycle;
+        csd.released = c->released;
         
         [releases setObject:@(csd.setNum) forKey:csd.setCode];
         
@@ -82,7 +85,7 @@ static struct cardSetData {
     NSMutableDictionary* dict = [NSMutableDictionary dictionary];
     for (CardSets* cs in cardSets)
     {
-        [dict setObject:@(YES) forKey:cs.settingsKey];
+        [dict setObject:@(cs.released) forKey:cs.settingsKey];
     }
     return dict;
 }
