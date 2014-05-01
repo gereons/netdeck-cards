1a57bdb11fb611946486223ee3f64f0e8cf2a34e
Gereon Steffens <gereon@steffens.org>
2014-05-01 12:20:17 +0200

    swip gesture re-enabled, fixes #92

--- NRDB/NRNavigationController.h
+++ NRDB/NRNavigationController.h
@@ -10,7 +10,7 @@
 
 @class DeckListViewController;
 
-@interface NRNavigationController : UINavigationController <UINavigationBarDelegate, UIAlertViewDelegate>
+@interface NRNavigationController : UINavigationController <UINavigationBarDelegate, UIAlertViewDelegate, UIGestureRecognizerDelegate>
 
 @property DeckListViewController* deckListViewController;
 
--- NRDB/NRNavigationController.m
+++ NRDB/NRNavigationController.m
@@ -13,6 +13,7 @@
 
 @property BOOL alertViewClicked;
 @property BOOL regularPop;
+@property BOOL swipePop;
 
 @end
 
@@ -22,56 +23,82 @@
 {
     [super viewDidLoad];
 
-    // disable swipe gesture
-    self.interactivePopGestureRecognizer.enabled = NO;
+    self.interactivePopGestureRecognizer.delegate = self;
     
-    // TODO: figure out how to handle and intercept swipe gesture correctly
-    // [self.interactivePopGestureRecognizer addTarget:self action:@selector(handlePopGesture:)];
+    [self.interactivePopGestureRecognizer addTarget:self action:@selector(handlePopGesture:)];
 }
 
-/*
 - (void)handlePopGesture:(UIGestureRecognizer *)gesture
 {
+    // NSLog(@"handle gesture, state=%d", gesture.state);
     if (gesture.state == UIGestureRecognizerStateEnded)
     {
-        NSLog(@"pop view");
+        // NSLog(@"end swipe, pop");
         [self popViewControllerAnimated:NO];
     }
 }
-*/
+
+- (BOOL)gestureRecognizerShouldBegin:(UIGestureRecognizer *)gestureRecognizer
+{
+    if (gestureRecognizer == self.interactivePopGestureRecognizer)
+    {
+        if (self.deckListViewController.deckChanged)
+        {
+            [self showAlert];
+            self.swipePop = NO;
+            return NO;
+        }
+    }
+    self.swipePop = YES;
+    // NSLog(@"swipe start ok");
+    return YES;
+}
 
 -(BOOL) navigationBar:(UINavigationBar *)navigationBar shouldPopItem:(UINavigationItem *)item
 {
+    if (self.swipePop)
+    {
+        // NSLog(@"should pop1: YES");
+        self.swipePop = NO;
+        return YES;
+    }
     if (self.regularPop)
     {
+        // NSLog(@"should pop2: YES");
         self.regularPop = NO;
         return YES;
     }
     if (self.alertViewClicked)
     {
+        // NSLog(@"should pop3: YES");
         self.alertViewClicked = NO;
         return YES;
     }
     
-    BOOL unsavedChanges = self.deckListViewController.deckChanged;
-    if (unsavedChanges)
+    if (self.deckListViewController.deckChanged)
     {
-        UIAlertView* alert = [[UIAlertView alloc]
+        // NSLog(@"should pop4: NO");
+        [self showAlert];
+        return NO;
+    }
+    else
+    {
+        // NSLog(@"should pop5: NO, pop self");
+        self.regularPop = YES;
+        [self popViewControllerAnimated:NO];
+        return NO;
+    }
+}
+
+-(void) showAlert
+{
+    UIAlertView* alert = [[UIAlertView alloc]
                           initWithTitle:nil
                           message:l10n(@"There are unsaved changes")
                           delegate:self
                           cancelButtonTitle:l10n(@"Cancel")
                           otherButtonTitles:l10n(@"Discard"), l10n(@"Save"), nil];
-        [alert show];
-        
-        return NO;
-    }
-    else
-    {
-        self.regularPop = YES;
-        [self popViewControllerAnimated:NO];
-        return NO;
-    }
+    [alert show];
 }
 
 -(void) alertView:(UIAlertView *)alertView didDismissWithButtonIndex:(NSInteger)buttonIndex
