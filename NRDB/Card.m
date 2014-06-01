294e26e830516f350f4e3a5f9a50865e94136e1f
Gereon Steffens <gereon@steffens.org>
2014-06-01 09:57:04 +0200

    remove old filter / filter header view

--- NRDB/Card.m
+++ NRDB/Card.m
@@ -235,8 +235,6 @@ static NSMutableArray* allCorpIdentities;
     return nil;
 }
 
-#define UIColorFromRGB(rgbValue) [UIColor colorWithRed:((float)((rgbValue & 0xFF0000) >> 16))/255.0 green:((float)((rgbValue & 0xFF00) >> 8))/255.0 blue:((float)(rgbValue & 0xFF))/255.0 alpha:1.0]
-
 -(NSUInteger) factionHexColor
 {
     switch (self.faction)
