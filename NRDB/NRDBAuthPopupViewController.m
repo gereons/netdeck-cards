b129cfe78635005a0128c9ca6edbce6701013c23
Gereon Steffens <gereon@steffens.org>
2014-10-04 13:54:48 +0200

    fix nrdb auth popover

--- NRDB/NRDBAuthPopupViewController.m
+++ NRDB/NRDBAuthPopupViewController.m
@@ -24,7 +24,8 @@ static NRDBAuthPopupViewController* popup;
     popup = [[NRDBAuthPopupViewController alloc] initWithNibName:@"NRDBAuthPopupViewController" bundle:nil];
     
     [vc presentViewController:popup animated:NO completion:nil];
-    popup.view.superview.bounds = CGRectMake(0, 0, 850, 466);
+    // popup.view.superview.bounds = CGRectMake(0, 0, 850, 466);
+    popup.preferredContentSize = CGSizeMake(850, 466);
 }
 
 - (id)initWithNibName:(NSString *)nibNameOrNil bundle:(NSBundle *)nibBundleOrNil
