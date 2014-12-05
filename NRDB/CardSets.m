03e8b0afe95288ada1aae82dfc1033215452b142
Gereon Steffens <gereon@steffens.org>
2014-12-05 20:51:24 +0100

    add breaker bay

--- NRDB/CardSets.m
+++ NRDB/CardSets.m
@@ -71,6 +71,7 @@ static struct cardSetData {
     
     // sansan
     { 23, "val", "", NRCycleSanSan, NO },
+    { 24, "bb", "", NRCycleSanSan, NO },
     
     { 0 }
 };
@@ -107,7 +108,7 @@ static struct cardSetData {
         @[  @2,  @3,  @4,  @5,  @6,  @7 ],
         @[  @9, @10, @11, @12, @13, @14 ],
         @[ @16, @17, @18, @19, @20, @21 ],
-        @[ @23 ]
+        @[ @23, @24 ]
     ];
     
     NSAssert(setGroups.count == setsPerGroup.count, @"set group mismatch");
