6b2459aa634d445be85ffd86e38936169fffc030
Gereon Steffens <gereon@steffens.org>
2015-09-20 16:04:33 +0200

    protect against rare crashed

--- NetDeck/NSArray+TwoD.m
+++ NetDeck/NSArray+TwoD.m
@@ -12,15 +12,22 @@
 
 -(id)objectAtIndexPath:(NSIndexPath *)indexPath
 {
-    if (indexPath == nil)
-    {
+    if (indexPath == nil) {
         return nil;
     }
     
-    NSAssert(indexPath.section < self.count, @"section out of range 0..%ld", (long)self.count);
+    NSAssert(indexPath.section < self.count, @"section %ld out of range 0..%ld", (long)indexPath.section, (long)self.count);
+    if (indexPath.section >= self.count) {
+        return nil;
+    }
+    
     NSArray* arr = self[indexPath.section];
     NSAssert([arr isKindOfClass:[NSArray class]], @"element at %ld is not an array", (long)indexPath.section);
-    NSAssert(indexPath.row < arr.count, @"row out of range 0..%ld", (long)arr.count);
+    NSAssert(indexPath.row < arr.count, @"row %ld out of range 0..%ld", (long)indexPath.row, (long)arr.count);
+    if (indexPath.row >= arr.count) {
+        return nil;
+    }
+    
     return arr[indexPath.row];
 }
 
