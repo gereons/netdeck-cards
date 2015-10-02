1cceee996ad06283e100ad8a93c97ad522389c1f
Gereon Steffens <gereon@steffens.org>
2015-10-02 20:54:51 +0200

    add monospaced Digits category

---
+++ NetDeck/UIFont+monospacedDigits.h
@@ -0,0 +1,14 @@
+//
+//  UIFont+monospacedDigits.h
+//  NetDeck
+//
+//  Created by Gereon Steffens on 02.10.15.
+//  Copyright © 2015 Gereon Steffens. All rights reserved.
+//
+
+@interface UIFont (monospacedDigits)
+
++(UIFont*) md_systemFontOfSize:(CGFloat)fontSize;
++(UIFont*) md_boldSystemFontOfSize:(CGFloat)fontSize;
+
+@end
---
+++ NetDeck/UIFont+monospacedDigits.m
@@ -0,0 +1,44 @@
+//
+//  UIFont+monospacedDigits.m
+//  NetDeck
+//
+//  Created by Gereon Steffens on 02.10.15.
+//  Copyright © 2015 Gereon Steffens. All rights reserved.
+//
+
+#import "UIFont+monospacedDigits.h"
+
+@implementation UIFont(monospaceDigits)
+
+static BOOL ios9;
+
++(void) initialize
+{
+    ios9 = [[UIFont class] respondsToSelector:@selector(monospacedDigitSystemFontOfSize:weight:)];
+}
+
++(UIFont*) md_systemFontOfSize:(CGFloat)fontSize
+{
+    if (ios9)
+    {
+        return [UIFont monospacedDigitSystemFontOfSize:fontSize weight:UIFontWeightRegular];
+    }
+    else
+    {
+        return [UIFont systemFontOfSize:fontSize];
+    }
+}
+
++(UIFont*) md_boldSystemFontOfSize:(CGFloat)fontSize
+{
+    if (ios9)
+    {
+        return [UIFont monospacedDigitSystemFontOfSize:fontSize weight:UIFontWeightBold];
+    }
+    else
+    {
+        return [UIFont boldSystemFontOfSize:fontSize];
+    }
+}
+
+@end
