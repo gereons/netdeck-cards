9570337fc8d0b75f2e1fd4a9ed80580b1442f66f
Gereon Steffens <gereon@steffens.org>
2016-06-27 08:16:38 +0200

    remove implicitly unwrapped optionals

--- NetDeck/TableData.swift
+++ NetDeck/TableData.swift
@@ -10,17 +10,16 @@ import Foundation
 
 @objc class TableData: NSObject {
 
-    var sections: NSArray!
-    var values: NSArray!
+    var sections: NSArray
+    var values: NSArray
     var collapsedSections: [Bool]?
     
     init(sections: NSArray, andValues values: NSArray) {
-        super.init()
-        
         assert(sections.count == values.count, "sections/values count mismatch")
         self.sections = sections
         self.values = values
         self.collapsedSections = nil
+        super.init()
     }
     
     convenience init(values: NSArray) {
