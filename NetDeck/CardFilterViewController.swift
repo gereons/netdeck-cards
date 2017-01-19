c1e38d27a64da1cb82b1718febab4e78014295d0
Gereon Steffens <gereon@steffens.org>
2017-01-19 07:45:32 +0100

    start rewriting CardFilterViewController

---
+++ NetDeck/CardFilterViewController.swift
@@ -0,0 +1,9 @@
+//
+//  CardFilterViewController.swift
+//  NetDeck
+//
+//  Created by Gereon Steffens on 18.01.17.
+//  Copyright © 2017 Gereon Steffens. All rights reserved.
+//
+
+import Foundation
