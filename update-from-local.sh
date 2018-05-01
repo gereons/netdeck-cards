9462c9e6d84c8aff143bba39f92d56d30869ab84
Gereon Steffens <gereon@steffens.org>
2018-05-01 18:26:53 +0200

    add 'update-from-local' script

---
+++ update-from-local.sh
@@ -0,0 +1,53 @@
+#!/bin/bash
+
+set -xv
+
+SERVER=http://localhost:8000
+CARDS_URL=$SERVER/api/2.0/public/cards
+CYCLES_URL=$SERVER/api/2.0/public/cycles
+PACKS_URL=$SERVER/api/2.0/public/packs
+LOCALE=""
+SUFFIX=""
+
+mkdir -p api/2.0
+rm -r tmp
+mkdir tmp
+
+curl -s $CARDS_URL -o tmp/cards.raw
+curl -s $CYCLES_URL -o tmp/cycles.raw
+curl -s $PACKS_URL -o tmp/packs.raw
+
+for raw in tmp/*.raw
+do
+    cat $raw |
+    sed 's/},/},\
+/g' |
+    sed 's/\([^\\]\)",/\1",\
+/g' |
+    sed 's/http:\/\/localhost:8000/https:\/\/netrunnerdb.com/' >$raw.2
+done
+
+for language in en de fr es pl kr jp zh
+do
+    SUFFIX="_$language"
+
+    cp tmp/cards.raw.2 api/2.0/cards$SUFFIX.json
+    cp tmp/cycles.raw.2 api/2.0/cycles$SUFFIX.json
+    cp tmp/packs.raw.2 api/2.0/packs$SUFFIX.json
+done
+
+rm -r tmp
+
+if git diff --quiet --exit-code
+then
+    echo no updates found
+else
+    git checkout test
+    echo updates found, committing
+    git add api/2.0/*
+    DATE=$(date +%Y-%m-%d)
+    git commit -m "update $DATE"
+    git push
+    git status
+    echo "don't forget to test and merge to master"
+fi
