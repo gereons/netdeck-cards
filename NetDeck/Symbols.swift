a45e81d0a29de2871ef6f905347bd9d752776f44
Gereon Steffens <gereon@steffens.org>
2018-05-07 20:04:27 +0200

    update netrunner.ttf

---
+++ NetDeck/Symbols.swift
@@ -0,0 +1,54 @@
+//
+//  Symbols.swift
+//  NetDeck
+//
+//  Created by Gereon Steffens on 05.05.18.
+//  Copyright © 2018 Gereon Steffens. All rights reserved.
+//
+
+import Foundation
+
+// symbol codepoints in the "netrunner.ttf" font
+
+enum Symbols: String {
+    case subroutine = "\u{e900}"
+    case mu3 = "\u{e901}"
+    case mu2 = "\u{e902}"
+    case mu1 = "\u{e903}"
+    case mu = "\u{e904}"
+    case trash = "\u{e905}"
+    case linkHalfTop = "\u{e906}"
+    case linkHalfBottom = "\u{e907}"
+    case link = "\u{e908}"
+    case click = "\u{e909}"
+    case recurringCredit = "\u{e90a}"
+    case credit = "\u{e90b}"
+    case spin = "\u{e90c}"
+    case sansan = "\u{e90d}"
+    case orderAndChaos = "\u{e90e}"
+    case lunar = "\u{e90f}"
+    case honorAndProfit = "\u{e910}"
+    case genesis = "\u{e911}"
+    case dataAndDestiny = "\u{e912}"
+    case creationAndControl = "\u{e913}"
+    case core = "\u{e914}"
+    case nbn = "\u{e915}"
+    case jinteki = "\u{e916}"
+    case weylandConsortium = "\u{e917}"
+    case haasBioroid = "\u{e918}"
+    case criminal = "\u{e919}"
+    case anarch = "\u{e91a}"
+    case shaper = "\u{e91b}"
+    case sunnyLebeau = "\u{e91c}"
+    case adam = "\u{e91d}"
+    case apex = "\u{e91e}"
+    case mumbad = "\u{e91f}"
+    case abr = "\u{e920}"
+    case flashpoint = "\u{e921}"
+    case redSand = "\u{e922}"
+    case terminalDirective = "\u{e923}"
+    case core2 = "\u{e924}"
+    case kitara = "\u{e925}"
+    case reignAndReverie = "\u{e926}"
+}
+
