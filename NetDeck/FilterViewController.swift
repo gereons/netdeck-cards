f3c9dcfe0677f7cba67788633430487073ba6275
Gereon Steffens <gereon@steffens.org>
2019-09-22 12:23:34 +0200

    pod update

--- NetDeck/FilterViewController.swift
+++ NetDeck/FilterViewController.swift
@@ -231,7 +231,7 @@ class FilterViewController: UIViewController, MultiSelectSegmentedControlDelegat
     }
     
     // MARK: - multi select delegate
-    func multiSelect(_ control: MultiSelectSegmentedControl, didChangeValue value: Bool, at index: UInt) {
+    func multiSelect(_ control: MultiSelectSegmentedControl, didChange value: Bool, at index: Int) {
         var set = Set<String>()
         
         if control.tag == Tags.type.rawValue {
