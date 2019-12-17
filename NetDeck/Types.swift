63b2e5a609ca22d1d8316e825b80e46ea8b03867
Gereon Steffens <gereon@steffens.org>
2019-12-17 20:31:11 +0100

    remove old code

--- NetDeck/Types.swift
+++ NetDeck/Types.swift
@@ -85,60 +85,6 @@ enum PackUsage: Int {
     case selected
 }
 
-/*
-enum MWL: Int, Comparable {
-    static func < (lhs: MWL, rhs: MWL) -> Bool {
-        return lhs.rawValue < rhs.rawValue
-    }
-
-    case none
-    case v1_0   // as of 2016-02-01
-    case v1_1   // as of 2016-08-01
-    case v1_2   // as of 2017-04-12
-    case v2_0   // as of 2017-10-01
-    case v2_1   // as of 2018-02-26
-    case v2_2   // as of 2018-09-06
-    case v3_0   // as of 2018-12-21
-    case v3_1   // as of 2019-02-22
-
-    static let latest = MWL.v3_1
-
-    // map from "mwl_code" values we get from the NRDB API
-    private static let codeMap: [String: MWL] = [
-        "NAPD_MWL_1.0": .v1_0,
-        "NAPD_MWL_1.1": .v1_1,
-        "NAPD_MWL_1.2": .v1_2,
-        "NAPD_MWL_2.0": .v2_0,
-        "NAPD_MWL_2.1": .v2_1,
-        "NAPD_MWL_2.2": .v2_2,
-        "standard-mwl-3-0": .v3_0,
-        "standard-mwl-3.1": .v3_1
-    ]
-
-    static func by(code: String) -> MWL {
-        return MWL.codeMap[code] ?? .none
-    }
-
-    var universalInfluence: Bool {
-        switch self {
-        case .none, .v1_2: return true
-        default: return false
-        }
-    }
-
-    // MARK: - settings values / titles
-    private static let all = [ MWL.none, .v1_0, .v1_1, .v1_2, .v2_0, .v2_1, .v2_2, .v3_0, v3_1 ]
-    private static let names = [ "Casual", "MWL v1.0", "MWL v1.1", "MWL v1.2", "MWL v2.0", "MWL v2.1", "MWL v2.2", "MWL v3.0", "MWL v3.1" ]
-    static func values() -> [Int] {
-        return all.map { $0.rawValue }
-    }
-
-    static func titles() -> [String] {
-        return all.map { names[$0.rawValue].localized() }
-    }
-}
-*/
-
 enum DeckLegality: Equatable {
     case casual
     case standard(mwl: Int)
