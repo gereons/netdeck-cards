81638fb7bce94479a4408ec956527f646f434c69
Gereon Steffens <gereon@steffens.org>
2021-03-13 10:50:04 +0100

    update builtin data

---
+++ update-builtin-data.sh
@@ -0,0 +1,7 @@
+#!/bin/sh
+
+cp ../netdeck-cards/api/2.0/rotations.json NetDeck
+cp ../netdeck-cards/api/2.0/mwl.json NetDeck
+cp ../netdeck-cards/api/2.0/cards_en.json NetDeck
+cp ../netdeck-cards/api/2.0/cycles_en.json NetDeck
+cp ../netdeck-cards/api/2.0/packs_en.json NetDeck
